5d8c31de1630b964482d7bdb549e06ab5daaf184
Vitaly <vitaliy@uxpressia.com>
2024-08-18 11:15:05 +0300

    version 0.1.7 (divide the lubrary into functional blocks)

--- src/ui3n-components.ts
+++ src/ui3n-components.ts
@@ -1,10 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import 'pinia';
 import type Ui3nButton from './components/ui3n-button/ui3n-button.vue';
 import type Ui3nChip from './components/ui3n-chip/ui3n-chip.vue';
 import type Ui3nCheckbox from './components/ui3n-checkbox/ui3n-checkbox.vue';
 import type Ui3nDialog from './components/ui3n-dialog/ui3n-dialog.vue';
-import type { Ui3nDialogComponentProps } from './components/ui3n-dialog/types';
 import type Ui3nDropFiles from './components/ui3n-drop-files/ui3n-drop-files.vue';
 import type Ui3nEmoji from './components/ui3n-emoji/ui3n-emoji.vue';
 import type Ui3nIcon from './components/ui3n-icon/ui3n-icon.vue';
@@ -12,12 +9,11 @@ import type Ui3nInput from './components/ui3n-input/ui3n-input.vue';
 import type Ui3nList from './components/ui3n-list/ui3n-list.vue';
 import type Ui3nMenu from './components/ui3n-menu/ui3n-menu.vue';
 import type Ui3nNotification from './components/ui3n-notification/ui3n-notification.vue';
-import type { Ui3nNotificationProps } from './components/ui3n-notification/types';
 import type Ui3nTableSortIcon from './components/ui3n-table/ui3n-table-sort-icon.vue';
 import type Ui3nTable from './components/ui3n-table/ui3n-table.vue';
+import type Ui3nTabs from './components/ui3n-tabs/ui3n-tabs.vue';
 import type Ui3nText from './components/ui3n-text/ui3n-text.vue';
 import type Ui3nVirtualScroll from './components/ui3n-virtual-scroll/ui3n-virtual-scroll.vue';
-import type Ui3nTabs from './components/ui3n-tabs/ui3n-tabs.vue';
 import type Ui3nBadge from './components/ui3n-badge/ui3n-badge.vue';
 import type Ui3nBreadcrumb from './components/ui3n-breadcrumbs/ui3n-breadcrumb.vue';
 import type Ui3nBreadcrumbs from './components/ui3n-breadcrumbs/ui3n-breadcrumbs.vue';
@@ -25,27 +21,14 @@ import type Ui3nSwitch from './components/ui3n-switch/ui3n-switch.vue';
 import type Ui3nStepLineBar from './components/ui3n-step-line-bar/ui3n-step-line-bar.vue';
 import type Ui3nProgressLinear from './components/ui3n-progress/ui3n-progress-linear.vue';
 import type Ui3nProgressCircular from './components/ui3n-progress/ui3n-progress-circular.vue';
+import type Ui3nTooltip from './components/ui3n-tooltip/ui3n-tooltip.vue';
 import type Ui3nHtml from './directives/ui3n-html';
 import type Ui3nClickOutside from './directives/ui3n-click-outside';
-import type { CbFunction, VueEventBus } from './plugins/vue-bus/types';
-import type { DialogInstance } from './plugins/dialogs/types';
 
-export * from './constants';
-export * from './utils';
-export * from './plugins';
 export * from './directives';
-export * from './components';
+export * from './ui3n-components';
 
 declare module 'vue' {
-  interface ComponentCustomProperties {
-    $openDialog: (params: Ui3nDialogComponentProps) => DialogInstance | undefined;
-    $createNotice: (params: Ui3nNotificationProps) => void;
-    $locale: string;
-    $tr: (key: string, placeholders?: Record<string, string>) => string;
-    $changeLocale: (lang: string) => void;
-    $emitter: VueEventBus;
-  }
-
   interface GlobalComponents {
     Ui3nBadge: typeof Ui3nBadge;
     Ui3nBreadcrumb: typeof Ui3nBreadcrumb;
@@ -70,29 +53,11 @@ declare module 'vue' {
     Ui3nTabs: typeof Ui3nTabs;
     Ui3nText: typeof Ui3nText;
     Ui3nVirtualScroll: typeof Ui3nVirtualScroll;
+    Ui3nTooltip: typeof Ui3nTooltip;
   }
 
   interface GlobalDirectives {
     Ui3nHtml: typeof Ui3nHtml;
     Ui3nClickOutside: typeof Ui3nClickOutside;
-  }
-}
-
-declare module 'pinia' {
-  export interface PiniaCustomProperties {
-    $openDialog: (params: Ui3nDialogComponentProps) => DialogInstance | undefined;
-    $createNotice: (params: Ui3nNotificationProps) => void;
-    $emitter: {
-      on: (type: string | symbol, handler: CbFunction) => void;
-      off: (type: string | symbol, handler?: CbFunction) => void;
-      emit: (type: string | symbol, arguments?: any) => void;
-      once: (type: string | symbol, handler: CbFunction) => void;
-      clear: () => void;
-    };
-    $i18n: {
-      locale: string;
-      changeLocale: (lang: string) => void;
-      tr: (key: string, placeholders?: Record<string, string>) => string;
-    };
   }
 }
